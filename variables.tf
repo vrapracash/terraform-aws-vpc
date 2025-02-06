f4ac891aeba4f81d8faaed1183eb0548368009c9
vrapracash <veeraprakash.bg@gmail.com>
2025-02-06 09:39:31 +0530

    script-update

--- variables.tf
+++ variables.tf
@@ -1,3 +1,68 @@
 variable "vpc_cidr" {
   #default = "0.0.0.0./16"
 }
+
+variable "project_name" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "enable_dns_hostnames" {
+    default = true
+}
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "vpc_tags" {
+    default = {}
+}
+
+variable "igw_tags" {
+    default = {}
+}
+
+variable "public_subnet_cidrs" {
+    type = list
+    validation{
+        condition = length(var.public_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid public subnet CIDRS"
+    }
+}
+
+variable "public_subnet_tags" {
+    default = {}
+}
+
+variable "private_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.private_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid prvate subnet CIDRS"
+    }
+}
+
+variable "private_subnet_tags" {
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.database_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid database subnets CIDRS"
+    }
+}
+
+variable "database_subnet_tags" {
+    default = {}
+}
+
+variable "db_subnet_group_tags" {
+    default = {}
+}
+
