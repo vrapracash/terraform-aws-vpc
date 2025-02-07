3937b427787d86d6028682042a41a5fc9ae1d8dd
veeraprakash <veeraprakash.bg@gmail.com>
2025-02-07 15:45:32 +0530

    script-update

--- variables.tf
+++ variables.tf
@@ -1,31 +1,24 @@
 variable "vpc_cidr" {
   #default = "0.0.0.0./16"
 }
-
 variable "project_name" {
     type = string
 }
-
 variable "environment" {
     type = string
 }
-
 variable "enable_dns_hostnames" {
     default = true
 }
-
 variable "common_tags" {
     default = {}
 }
-
 variable "vpc_tags" {
     default = {}
 }
-
 variable "igw_tags" {
     default = {}
 }
-
 variable "public_subnet_cidrs" {
     type = list
     validation{
@@ -33,11 +26,9 @@ variable "public_subnet_cidrs" {
         error_message = "Please provide 2 valid public subnet CIDRS"
     }
 }
-
 variable "public_subnet_tags" {
     default = {}
 }
-
 variable "private_subnet_cidrs" {
     type = list
     validation {
@@ -45,11 +36,9 @@ variable "private_subnet_cidrs" {
         error_message = "Please provide 2 valid prvate subnet CIDRS"
     }
 }
-
 variable "private_subnet_tags" {
     default = {}
 }
-
 variable "database_subnet_cidrs" {
     type = list
     validation {
@@ -57,12 +46,28 @@ variable "database_subnet_cidrs" {
         error_message = "Please provide 2 valid database subnets CIDRS"
     }
 }
-
 variable "database_subnet_tags" {
     default = {}
 }
-
 variable "db_subnet_group_tags" {
     default = {}
 }
-
+variable "nat_gateway_tags" {
+    default = {}
+}
+variable "public_route_table_tags" {
+    default  = {}
+}
+variable "private_route_table_tags" {
+    default = {}
+}
+variable "database_route_table_tags" {
+    default = {}
+}
+variable "is_peering_required" {
+    type = bool
+    default = false
+}
+variable "vpc_peering_tags" {
+    default = {}
+}
