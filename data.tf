66e0e5002ceefa796279873d8fc527ea3ad85b7f
vrapracash <veeraprakash.bg@gmail.com>
2025-09-02 15:59:49 +0530

    script-update

--- data.tf
+++ data.tf
@@ -2,9 +2,9 @@
 #   state = "available"
 # }
 
-data  "aws_availability_zones" "available" {
-  state = "available"
-}
+# data  "aws_availability_zones" "available" {
+#   state = "available"
+# }
 
 # data "aws_vpc" "default" {
 #   default = true
@@ -12,11 +12,27 @@ data  "aws_availability_zones" "available" {
 # data "aws_vpc" "selected" {
 #   id = var.vpc_id_default
 # }
-data "aws_vpc" "default" {
-  id = var.vpc_id_default
+# data "aws_vpc" "default" {
+#   id = var.vpc_id_default
+# }
+# data "aws_route_table" "main" {
+#   vpc_id = var.vpc_id_default
+#   filter {
+#     name = "association.main"
+#     values = ["true"]
+#   }
+# }
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
-data "aws_route_table" "main" {
-  vpc_id = var.vpc_id_default
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_route_table" "main"{
+  vpc_id = data.aws_vpc.default.id
   filter {
     name = "association.main"
     values = ["true"]
