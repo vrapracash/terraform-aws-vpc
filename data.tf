65143628ccbc3b159b514e5164ff56bb93239ff0
vrapracash <veeraprakash.bg@gmail.com>
2025-09-02 16:08:24 +0530

    script-update

--- data.tf
+++ data.tf
@@ -9,9 +9,9 @@
 # data "aws_vpc" "default" {
 #   default = true
 # }
-data "aws_vpc" "selected" {
-  id = var.vpc_id_default
-}
+# data "aws_vpc" "selected" {
+#   id = var.vpc_id_default
+# }
 data "aws_vpc" "default" {
   id = var.vpc_id_default
 }
@@ -27,9 +27,9 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+# data "aws_vpc" "default" {
+#   default = true
+# }
 
 data "aws_route_table" "main"{
   vpc_id = data.aws_vpc.default.id
