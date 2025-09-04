ad5c09e418d2caefb5e564b3bffc12b7dc150324
vrapracash <veeraprakash.bg@gmail.com>
2025-09-04 17:02:16 +0530

    variable

--- data.tf
+++ data.tf
@@ -27,9 +27,9 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+# data "aws_vpc" "default" {
+#   default = true
+# }
 
 data "aws_route_table" "main"{
   vpc_id = data.aws_vpc.default.id
