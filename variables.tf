ab3fdcf76b3435d5e97a0c368c1a9bb5b355e034
vrapracash <veeraprakash.bg@gmail.com>
2025-09-04 16:47:49 +0530

    variable

--- variables.tf
+++ variables.tf
@@ -7,28 +7,34 @@ variable "environment" {
 }
 
 variable "vpc_cidr" {
-    #default      = "10.0.0.0/16"
+    #default = "10.0.0.0/16"
 }
 
 variable "enable_dns_hostnames" {
-    default     = true
+    default = true
 }
+
+# Optional
 variable "common_tags" {
     default = {}
 }
+
 variable "vpc_tags" {
     default = {}
 }
+
 variable "igw_tags" {
     default = {}
 }
+
 variable "public_subnet_cidrs" {
     type = list
     validation {
-        condition =  length(var.public_subnet_cidrs) == 2
-        error_message = "please provide 2 public subnet cidrs"
+        condition = length(var.public_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid public subnet CIDR"
     }
 }
+
 variable "public_subnet_tags" {
     default = {}
 }
@@ -36,20 +42,23 @@ variable "public_subnet_tags" {
 variable "private_subnet_cidrs" {
     type = list
     validation {
-        condition =  length(var.private_subnet_cidrs) == 2
-        error_message = "please provide 2 private subnet cidrs"
+        condition = length(var.private_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid private subnet CIDR"
     }
 }
+
 variable "private_subnet_tags" {
     default = {}
 }
+
 variable "database_subnet_cidrs" {
     type = list
     validation {
-        condition =  length(var.database_subnet_cidrs) == 2
-        error_message = "please provide 2 database subnet cidrs"
+        condition = length(var.database_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid database subnet CIDR"
     }
 }
+
 variable "database_subnet_tags" {
     default = {}
 }
@@ -58,7 +67,7 @@ variable "db_subnet_group_tags" {
     default = {}
 }
 
-variable "nat_gatewat_tags" {
+variable "nat_gateway_tags" {
     default = {}
 }
 
