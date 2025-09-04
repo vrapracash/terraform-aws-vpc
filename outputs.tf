f6cf73aa156c0ec9b4cf36c3ed3fb1b0ce5fd06e
vrapracash <veeraprakash.bg@gmail.com>
2025-09-04 17:42:52 +0530

    script-modified

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,44 @@
-# vpc_id check with terraform
-output "vpc_id" { 
-    value = aws_vpc.main.id
-}
-#Not able to find the vpc here
+# # vpc_id check with terraform
+# output "vpc_id" { 
+#     value = aws_vpc.main.id
+# }
+# #Not able to find the vpc here
+# # output "default_vpc_info" {
+# #     value = data.aws_vpc.default
+# # }
+
+# # az_info name check with terraform
+# output "az_info" { 
+#     value = data.aws_availability_zones.available
+# }
+
 # output "default_vpc_info" {
 #     value = data.aws_vpc.default
 # }
 
-# az_info name check with terraform
-output "az_info" { 
-    value = data.aws_availability_zones.available
+# output "aws_route_table_info" {
+#     value = data.aws_route_table.main
+# }
+# output "public_subnet_ids"{
+#   value = aws_subnet.public[*].id
+# }
+
+# output "private_subnet_ids"{
+#   value = aws_subnet.private[*].id
+# }
+
+# output "database_subnet_ids"{
+#   value = aws_subnet.database[*].id
+# }
+
+# output "database_subnet_group_name"{
+#   value = aws_db_subnet_group.default.name
+# }
+
+output  "vpc_id" {
+  value       = aws_vpc.main.id
 }
 
-output "default_vpc_info" {
-    value = data.aws_vpc.default
-}
-
-output "aws_route_table_info" {
-    value = data.aws_route_table.main
-}
 output "public_subnet_ids"{
   value = aws_subnet.public[*].id
 }
@@ -34,3 +54,15 @@ output "database_subnet_ids"{
 output "database_subnet_group_name"{
   value = aws_db_subnet_group.default.name
 }
+
+# output "az_info" {
+#     value = data.aws_availability_zones.available
+# }
+
+# output "default_vpc_info" {
+#   value = data.aws_vpc.default
+# }
+
+# output "main_route_table_info" {
+#   value = data.aws_route_table.main
+# }
