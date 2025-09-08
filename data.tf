f476a3cf3d4006870e415a3aadd2f004ff4009f1
vrapracash <veeraprakash.bg@gmail.com>
2025-09-08 18:30:22 +0530

    default-vpc-changed

--- data.tf
+++ data.tf
@@ -1,27 +1,6 @@
-# data "aws_availability_zones" "available" {
-#   state = "available"
-# }
-
-# data  "aws_availability_zones" "available" {
-#   state = "available"
-# }
-
-# data "aws_vpc" "default" {
-#   default = true
-# }
-# data "aws_vpc" "selected" {
-#   id = var.vpc_id_default
-# }
 data "aws_vpc" "default" {
   id = var.vpc_id_default
 }
-# data "aws_route_table" "main" {
-#   vpc_id = var.vpc_id_default
-#   filter {
-#     name = "association.main"
-#     values = ["true"]
-#   }
-# }
 
 data "aws_availability_zones" "available" {
   state = "available"
